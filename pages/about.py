56d8666fa3cfc8dc2780932410fb55ba04d841e0
milind soorya <milindsoorya@gmail.com>
2022-07-28 13:33:32 +0100

    add streamlit

---
+++ pages/about.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title('Denoising Ai (Dino_AI) 🧙🏻')
+st.write(
+    'This app enables you to denoise your image using various SOTA based methods.')
+st.markdown(
+    """The code for this project can be seen here **[GitHub](https://github.com/milindsoorya/dino-ai.git)**.""")
+
+
+st.markdown(
+    """
+    ## Version 0.1.0
+
+    _Release date: July 28, 2022
+
+    **Highlights**
+
+    - 📷 Added pretrained RIDNet model.
+    - ⚡️ New pages added: About
+    - ⚾️ Check notebooks for models in testing. **[notebooks](github.com/milindsoorya/dino-ai/blob/master/notebooks/)**
+
+    """
+)
